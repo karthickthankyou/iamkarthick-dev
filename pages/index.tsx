379f7515e53d2f51144bb0cd613e45805914fca4
Karthick Ragavendran <karthickragavendran@Karthicks-MacBook-Air.local>
2022-03-23 18:17:38 +0530

    updated footer to trigger deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
         <link rel='icon' href='/favicon.ico' />
       </Head>
       <main>Karthick Ragavendran</main>
-      <footer>footer</footer>
+      <footer>footer text</footer>
     </div>
   )
 }
