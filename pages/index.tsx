9cd93eb4eed3d328f19e0e0d0f60e1f911b1f3a0
Karthick Ragavendran <karthickthankyou@gmail.com>
2022-03-28 11:11:59 +0530

    testing deployment

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ const Home: NextPage = () => {
       <main>
         <div>Karthick Ragavendran</div>
         <div>Web developer.</div>
+        <div>React</div>
       </main>
       <footer>footer text</footer>
     </div>
